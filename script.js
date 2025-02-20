3579f087655ca03702685518b2b1cc224a3d6323
Mélanie <melanie.taussat@laposte.net>
2025-02-20 11:08:11 +0100

    remaniement du script du quizz, toujours pas opti mais répond aux consignes

--- script.js
+++ script.js
@@ -56,7 +56,101 @@ miniature.forEach(mini => {
 })
 
 // Script pour la partie Quizz 
-// /!\ Merite d'etre remanier car pas du tout opti. /!\
+
+// Pour la question 1 
+// Recupération des boutons de reponses 
+let bouton1= document.getElementById("opt1"); 
+let bouton2 = document.getElementById("opt2"); 
+// recuperation de la div vide pour afficher le score 
+let scoreVide= document.getElementById("score")
+// Initialisation du score 
+let score = 0 
+//  Création de la fonction qui sera déclenché avec le addEventListener,
+//  prennant en compte trois paramètres: le bouton cliqué par l'utlisateur, le bouton qui disparait et si la reponse est correct ou pas
+function afficherReponse1(boutonClique , boutonCache, estCorrect){
+    // bouton qui sera caché
+    boutonCache.style.display = "none";
+    // changer le bouton et le texte du bouton cliqué 
+    boutonClique.classList.remove("btnQuizz")
+    if (estCorrect){
+        score ++ 
+        boutonClique.classList.add("btnQuizzCorrect")
+        boutonClique.innerHTML = "✅ Correct ! Chez Quattre Pattes, 85% des dons sont directement utilisés pour les animaux."
+    }else{
+        boutonClique.classList.add("btnQuizzFalse"); 
+        boutonClique.innerHTML = "❌ Faux ! Chez Quattre Pattes, 85% des dons sont directement utilisés pour les animaux. Seuls 15% servent aux frais de fonctionnement essentiels."
+    }
+    scoreVide.innerHTML= "Votre score est de : " + score + "/3"
+}
+// ajout de l'appel du declenchement de la fonction 
+bouton1.addEventListener("click", () =>{
+    // si l'utilisateur clique sur le bouton de mauvaise réponse 
+    afficherReponse1(bouton1, bouton2, false)
+})
+bouton2.addEventListener("click", ()=>{
+    // si l'utilisateur clique sur la bonne reponse 
+    afficherReponse1(bouton1, bouton2, true)
+})
+
+// Pour la question 2: 
+let bouton2_1= document.getElementById("opt3")
+let bouton2_2= document.getElementById("opt4")
+function afficherReponse1(boutonClique , boutonCache, estCorrect){
+    // bouton qui sera caché
+    boutonCache.style.display = "none";
+    // changer le bouton et le texte du bouton cliqué 
+    boutonClique.classList.remove("btnQuizz")
+    if (estCorrect){
+        score ++ 
+        boutonClique.classList.add("btnQuizzCorrect")
+        boutonClique.innerHTML = " ✅ Faux! Nous acceuillons tout les animaux, quel que soit leur état de santé. Chaque vie compte!" 
+    }else{
+        boutonClique.classList.add("btnQuizzFalse"); 
+        boutonClique.innerHTML = " ❌ Faux ! Nous acceuillons tout les animaux, quel que soit leur état de santé. Chaque vie compte! "
+    }
+    scoreVide.innerHTML= "Votre score est de : " + score + "/3"
+}
+// ajout de l'appel du declenchement de la fonction 
+bouton2_1.addEventListener("click", () =>{
+    // si l'utilisateur clique sur le bouton de mauvaise réponse 
+    afficherReponse1(bouton2_1, bouton2_2, false)
+})
+bouton2_2.addEventListener("click", ()=>{
+    // si l'utilisateur clique sur la bonne reponse 
+    afficherReponse1(bouton2_1, bouton2_2, true)
+})
+
+// Troisième question 
+let bouton3_1= document.getElementById("opt5")
+let bouton3_2= document.getElementById("opt6")
+function afficherReponse1(boutonClique , boutonCache, estCorrect){
+    // bouton qui sera caché
+    boutonCache.style.display = "none";
+    // changer le bouton et le texte du bouton cliqué 
+    boutonClique.classList.remove("btnQuizz")
+    if (estCorrect){
+        score ++ 
+        boutonClique.classList.add("btnQuizzCorrect")
+        boutonClique.innerHTML = " ✅ C'est faux en effet ! Au vu des demande croissante, n'hesitez pas à donner!" 
+    }else{
+        boutonClique.classList.add("btnQuizzFalse"); 
+        boutonClique.innerHTML = "❌ Faux! Les besoins sont immenses et constants. Chaque don est précieux pour sauver plus d’animaux."  
+    }
+    scoreVide.innerHTML= "Votre score est de : " + score + "/3"
+}
+// ajout de l'appel du declenchement de la fonction 
+bouton3_1.addEventListener("click", () =>{
+    // si l'utilisateur clique sur le bouton de mauvaise réponse 
+    afficherReponse1(bouton3_1, bouton3_2, false)
+})
+bouton3_2.addEventListener("click", ()=>{
+    // si l'utilisateur clique sur la bonne reponse 
+    afficherReponse1(bouton3_1, bouton3_2, true)
+})
+
+
+
+/*
 
 // recuperation des boutons correct/incorrect de la question 1 
 let mauvaise_reponse1 = document.getElementById("opt1")
@@ -102,7 +196,7 @@ bonne_reponse3.addEventListener("click", ()=>{
 })
 
 
-
+*/
 
 
 
